62d6f2c8d5c7d91901df3de072ee7cc0fc605b11
jeckbjy <jinyuan.bian@zenjoy.net>
2018-05-16 16:21:40 +0800

    table shell

--- tools/table-gen/build.sh
+++ tools/table-gen/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-${DIR}/tool-table-gen/bin/build.sh -c ./conf.yaml
-
+pushd $DIR > /dev/null
+./tool-table-gen/bin/build.sh
+popd > /dev/null
